9f4b14258a8f5ef7a31807ee0f4f3b2ea2581064
cody1213 <cody_brumfield@hotmail.com>
2013-12-23 21:52:20 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,123 @@
-twism
+twism (alpha)
 =====
 
 A clickable world map jQuery plugin
+###Quick Instructions
+Include jQuery and the jQuery twism plugin:
+
+	<script src="http://code.jquery.com/jquery.js"></script>
+	<script src="jquery.twism.js"></script>
+Create a container:
+
+	<div id="worldmap"></div>
+
+Intialize the plugin:
+
+	$('#worldmap').twism();
+	
+:
+
+###Usage
+The basic usage is 
+
+	selector.twism([method], [options], [callback]);
+	
+Everything is optional. The default creates with the default options and has no callback.  
+
+You can pass one of three methods: create, destroy, and setCountry. Create and destroy add and remove maps while the setCountry modifies a single country.
+	
+	$('#worldmap').twism("destroy");
+
+The create and setCountry commands take options. The full list of options follows but the setCountry command only accepts color, border, and borderWidth.
+ 	
+ 	$('#worldmap').twism("create", {
+    	border: "red",
+        individualCountrySettings: [{
+            name: "cn",
+        	color: "red"
+    	}, {
+    		name: "fr",
+    		color: "blue"
+    	}, {
+    		name: "us",
+    		color: "white"
+    	}]
+    });
+
+All of the methods accept a callback
+    
+	$('#worldmap').twism("setCountry", {
+          name: "ca",
+          color: "blue"
+    }, function() {
+    	alert("Callback!");
+    });
+###Options
+
+Include Antarctica (default: false)
+
+	antarctica: true
+
+Set background color of countries (default: #A9DA8A) 
+	
+	color: "#A9DA8A"
+
+Set the default color for the country borders (default: white)
+	
+	border: "white"
+
+Set the border width (default: 2):
+
+	borderWidth: 2
+	
+Set the background color (default: "#4af")
+
+	backgroundColor: "#4af"
+
+Set the height (default or null: 100% of the container)
+	
+	height: 300px
+
+Set the width (default or null: 100% of the container): 
+	
+	width: 500px
+
+Set a click function (default alerts with the country code)
+
+	click: function(country) {
+    	alert(country);
+	}
+
+Set a hover color (default: #BB0029)
+	
+	hoverColor: "#BB0029"
+	
+Set the hover border (default: "yellow") 
+	
+	hoverBorder: "yellow"
+
+Include Taiwan, HK, and Macao in the PRC (China) (default: true)
+
+	littleRedBook: false,
+
+Hide any countries (array of 2-digit country codes)
+
+	hideCountries: ["cn","ca","nz"]
+
+Set Individual Settings for countries	
+
+
+	individualCountrySettings: [
+		{
+			name: "us",
+			color: "white",
+			border: "green"
+			borderWidth: 5
+		},
+		{
+			name: "fr",
+			color: "blue",
+			border: "orange"
+			borderWidth: 10
+		},
+	]
---
+++ jquery.twism.js
@@ -0,0 +1,179 @@
+(function($) {
+    $.fn.twism = function() {
+        var self = this;
+        //define constants
+        
+        if (typeof arguments[0] === "string") {
+          action = arguments[0];
+          options = arguments[1] || null;
+          callback = arguments[1] || null;
+        } else if (typeof arguments[0] === "object") {
+          action = "create";
+          options = arguments[0];
+          callback = arguments[1] || null;
+        } else if (typeof arguments[0] === "function") {
+          action = "create";
+          options = null;
+          callback = arguments[0];
+        } else {
+          action = "create";
+          options = null;
+          callback = null;
+        }
+        
+        var destroy = function(options, callback) {
+            var that = self;
+            self.remove();
+            if (typeof callback === "function") callback();
+            return;
+        }
+        var create = function(options, callback) {
+            var settings = $.extend({
+                // These are the defaults.
+                antarctica: false,
+                color: "#A9DA8A",
+                border: "white",
+                backgroundColor: "#4af",
+                borderWidth: "2",
+                height: null,//default is actually 100%
+                width: null, //default is actually 100%
+                click: function(state) {
+                    alert(state);
+                },
+                hover: function() {
+                  return;
+                },
+                hoverColor: "#BB0029",
+                hoverBorder: "yellow",
+                littleRedBook: true,
+                hideCountries: null,
+                individualCountrySettings: null
+            }, options);
+            console.log(settings);
+            // add the SVG to the div
+            var that = self;
+            that.css({
+                background: settings.backgroundColor,
+                width: (settings.width || "100%"),
+                height: (settings.height || "100%")
+            });
+            var url = (settings.antarctica) ? 'world-map-with-antarctica.svg' : 'world-map.svg';
+            that.load(url, null, function(e) {
+                $("svg", that).attr({
+                    height: that.height(),
+                    width: that.width()
+                });
+                var hiddens = settings.hideCountries;
+                for (i in hiddens) {
+                    $("path#" + hiddens[i], that).remove();
+                };
+                
+                $("svg path", that).css({
+                  fill: settings.color,
+                  stroke: settings.border,
+                  strokeWidth: settings.borderWidth
+                });
+                var ics = settings.individualCountrySettings;
+                for (i in ics) {
+                  $("svg path#"+ics[i].name, that).css({
+                    fill: ics[i].color || settings.color,
+                    stroke: ics[i].border || settings.border
+                  }); 
+                }
+                $("svg", that).on("click", function(e) {
+                    lastclicked = $(e.target).attr("id");
+                    settings.click(lastclicked);
+                });
+
+                $("svg path", that).on("mouseover", function(e) {
+                    var icss = $.map(ics, function(o) { return o["name"]; })
+                    var country = $(e.target).attr("id");
+                    
+                    if (icss.indexOf(country) > -1) {
+                      for (i in ics) {
+                        if (ics[i].name == country) {
+                          $("svg path#"+ics[i].name, that).css({
+                            fill: ics[i].hoverColor || settings.hoverColor,
+                            stroke: ics[i].hoverBorder || settings.hoverBorder
+                          }); 
+                        }
+                      }
+                    } else {
+                      $('path#' + country, that).css({
+                          "fill": settings.hoverColor,
+                          "stroke": settings.hoverBorder
+                      });
+                    }
+                    
+                    if (settings.littleRedBook) {
+                        if (country == "cn" || country == "tw" || country == "hk" || country == "mc") {
+                            $('path#tw, path#cn, path#hk, path#mc').css({
+                                "fill": settings.hoverColor,
+                                "stroke": settings.hoverBorder
+                            });
+                        }
+                    }
+                });
+
+                $("svg path", that).on("mouseout", function(e) {
+                    var country = $(e.target).attr("id");
+                    $('path#' + country, that).css({
+                        "fill": settings.color,
+                        "stroke": settings.border
+                    });
+                    for (i in ics) {
+                      $("svg path#"+ics[i].name, that).css({
+                        fill: ics[i].color || settings.color,
+                        stroke: ics[i].border || settings.border
+                      }); 
+                    }
+
+                    if (settings.littleRedBook) {
+                        if (country == "cn" || country == "tw" || country == "hk" || country == "mc") {
+                            $('path#tw, path#cn, path#hk, path#mc').css({
+                                "fill": settings.color,
+                                "stroke": settings.border
+                            });
+                        }
+                    }
+                });
+
+            });
+            
+            if (typeof callback === "function") callback();
+            return that;
+        }
+        
+        var setCountrySettings = function(options) {
+          if (options.color) {
+            $("svg path#"+options.name).css({
+              fill: options.color
+            });  
+          }
+          if (options.border) {
+            $("svg path#"+options.name).css({
+              stroke: options.border
+            });  
+          }
+          if (options.borderWidth) {
+            $("svg path#"+options.name).css({
+              strokeWidth: options.borderWidth
+            });  
+          }  
+        };
+
+        switch (action) {
+        case "create":
+            return create(options, callback);
+            break;
+        case "destroy":
+            return destroy(options, callback);
+            break;
+        case "setCountry":
+            return setCountrySettings(options);
+            break;
+        default:
+            return create(options, callback);
+        }
+    };
+}(jQuery));
